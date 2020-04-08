2c20caa7d86e33fe39769f839e2258b6a5f7c20e
jnele <elianmalakias14@gmail.com>
2020-04-08 13:50:09 -0400

    HITO 2

--- HITO 2 todo/Examen2/Main.java
+++ HITO 2 todo/Examen2/Main.java
@@ -1,36 +1,27 @@
 package Examen2;
+
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-//        Policia po1 = new Policia();
-//        Policia po2 = new Policia();
-//        po1.Leer();
-//        //  po2.Leer();
-//
-//        Profesor pf1 = new Profesor();
-//        Profesor pf2 = new Profesor();
-//        pf1.Leer();
-////        pf2.Leer();
-////        pf2.Mostrar();
-//
-//        System.out.println(pf1.verificaCiudad(po1.getDestinos()));
-
-        Departamento dep=new Departamento();
-        dep.Leer();
-         dep.Mostrar();
-        System.out.println();
-
-        Policia po1= new Policia();
+        Policia po1 = new Policia();
+        Policia po2 = new Policia();
         po1.Leer();
-        po1.Mostrar();
-        System.out.println();
+        //  po2.Leer();
 
-        System.out.println(po1.verificaDepartamento(po1.getDestinos(),dep.getNombreDep()));
+        Profesor pf1 = new Profesor();
+        Profesor pf2 = new Profesor();
+        pf1.Leer();
+//        pf2.Leer();
+//        pf2.Mostrar();
 
 
+        System.out.println(pf1.verificaCiudad(po1.getDestinos()));
 
 
 
     }
+
+
+
 }
--- HITO 2 todo/Examen2/Policia.java
+++ HITO 2 todo/Examen2/Policia.java
@@ -1,88 +1,46 @@
 package Examen2;
 
-import java.util.Arrays;
-
-public class Policia implements LeerInterface{//extends Persona{
-    private String[] nombrePolicia;
-    private String[] apellidoPolicia;
-    private int n;
-    private String[] turno;
-    private int[] nroDestinos;
+public class Policia extends Persona {
+    private String turno;
+    private int nroDestinos;
     private String[] destinos;
 
     public Policia() {
-       this.n=0;
+        this.turno = "";
+        this.nroDestinos = 0;
+
     }
+
 
     public void Leer(){
-        //super.Leer();
-        System.out.printf("Ingrese cantidad de policias: ");
-        this.n = LEER.nextInt();
-        this.nombrePolicia = new String[this.n];
-        this.apellidoPolicia = new String[this.n];
-        this.turno = new String[this.n];
-        this.nroDestinos = new int[this.n];
-        //this.destinos = new String[this.n];
+        super.Leer();
+        System.out.printf("Ingrese turno: ");
+        this.turno = LEER.next();
+        System.out.printf("Ingrese nro Destinos: ");
+        this.nroDestinos = LEER.nextInt();
+        this.destinos = new String[this.nroDestinos];
 
-        for(int i=0; i<this.n; i++){
-            System.out.printf("Ingrese nombre: ");
-            this.nombrePolicia[i] = LEER.next();
-
-            System.out.printf("Ingrese apellido: ");
-            this.apellidoPolicia[i] = LEER.next();
-
-            System.out.printf("Ingrese turno: ");
-            this.turno[i] = LEER.next();
-
-            System.out.printf("Ingrese nroDestinos: ");
-            this.nroDestinos[i] = LEER.nextInt();
-            this.destinos = new String[this.nroDestinos[i]];
-            for(int j=0; j<this.nroDestinos[i]; j++){
-                System.out.printf("Ingrese destino: ");
-                this.destinos[j] = LEER.next();
-                System.out.println();
-
-             }
-
-
+        for(int i=0; i<this.nroDestinos; i++){
+            System.out.printf("Ingrese destino: ");
+            this.destinos[i] = LEER.next();
+            System.out.println();
         }
     }
+
     public void Mostrar(){
-        //super.Mostrar();
-        //System.out.printf("cantidad de policias: %d,turno: %s,nro destinos: %d %s",this.n, this.turno,this.nroDestinos, "\n");
-        for(int i=0; i<this.n; i++){
-            System.out.printf("cantidad de policias: %d,nombre policia: %s,apellido policia: %s,turno: %s,nroDestino: %d %s",this.n,this.nombrePolicia[i],this.apellidoPolicia[i], this.turno[i],this.nroDestinos[i],"\n");
-            for(int j=0; j<this.nroDestinos[i]; j++){
-                System.out.printf("Destino: %d: %s",j,this.destinos[j]);//Destinos: 0 Cochabamba
-
-            }
+        super.Mostrar();
+        System.out.printf("Turno: %s,Nro Destinos: %d %s", this.turno,this.nroDestinos, "\n");
+        for(int j=0; j<this.nroDestinos; j++){
+            System.out.printf("Destino: %d: %s",j,this.destinos[j]);//Destinos: 0 Cochabamba
 
         }
-
     }
-
-
     public String[] getDestinos(){
         return this.destinos;
     }
 
 
-    public String verificaDepartamento(String[] destinos,String[] departamento){
-        int conta=0;
-        String res = "";
-        for(int i=0; i<destinos.length; i++){
-
-            for(int j=0; j<departamento.length; j++){
-                if (destinos.length == departamento.length){
-                    res=String.valueOf(conta++);
-                }
-            }
-        }
-        return res;
-    }
-
 
 }
-
 
 
